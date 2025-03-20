0facf8e983730b9e380fcddc50db06bcd5ffcf8a
Niyaz <niazsajdullin@gmail.com>
2025-03-20 23:14:14 +0500

    fix BenchmarkRunner
    redis, mysql - localhost -> container-name
    benchmark result -> /result mount with volume

--- src/main/java/benchmark/BenchmarkRunner.java
+++ src/main/java/benchmark/BenchmarkRunner.java
@@ -5,17 +5,27 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.io.File;
+
 public class BenchmarkRunner {
     public static void main(String[] args) throws Exception {
-        Options options = new OptionsBuilder()
-                .include(BenchmarkDB.class.getSimpleName())
-                .forks(1)
-                .warmupIterations(1)
-                .measurementIterations(3)
-                .result("benchmarkResult.json")
-                .resultFormat(ResultFormatType.JSON)
-                .build();
+        boolean result = new File("result").mkdirs();
 
-        new Runner(options).run();
+        if (result) {
+
+            Options options = new OptionsBuilder()
+                    .include(BenchmarkDB.class.getSimpleName())
+                    .forks(1)
+                    .warmupIterations(1)
+                    .measurementIterations(1)
+                    .result("/result/benchmarkResult.json")
+                    .resultFormat(ResultFormatType.JSON)
+                    .build();
+
+            new Runner(options).run();
+        }
+        else {
+            System.out.println("Could not create result directory");
+        }
     }
 }
