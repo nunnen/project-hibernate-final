ab05af82ce93fb7471253d3172d5ebf763bec340
Niyaz <niazsajdullin@gmail.com>
2025-03-21 13:12:40 +0500

    small fixes

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -15,7 +15,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import redis.CityCountry;
 import redis.Language;
 
@@ -141,21 +140,11 @@ public class Main {
 
     public SessionFactory prepareRelationalDb() {
         final SessionFactory sessionFactory;
-//        Properties properties = new Properties();
-//        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-//        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
-//        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/world");
-//        properties.put(Environment.USER, "root");
-//        properties.put(Environment.PASS, "root");
-//        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-//        properties.put(Environment.HBM2DDL_AUTO, "validate");
-//        properties.put(Environment.STATEMENT_BATCH_SIZE, "100");
 
         sessionFactory = new Configuration()
                 .addAnnotatedClass(City.class)
                 .addAnnotatedClass(Country.class)
                 .addAnnotatedClass(CountryLanguage.class)
-//                .addProperties(properties)
                 .buildSessionFactory();
         return sessionFactory;
     }
